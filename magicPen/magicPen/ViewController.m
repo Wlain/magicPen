7add036233ca93053100d2eef48182accabce3fc
陈威彪 <cwb2@meitu.com>
2020-03-20 17:25:17 +0800

    【feature】配置OpenGL环境

--- magicPen/magicPen/ViewController.m
+++ magicPen/magicPen/ViewController.m
@@ -9,21 +9,30 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(nonatomic, strong) EAGLContext *context;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];
+    if (self.context == nil) {
+        self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    }
+    if (self.context == nil) {
+        NSLog(@"Failed to create OpenGLES context");
+    }
+    GLKView *view = (GLKView*)self.view;
+    view.context = self.context;
+    // 初始化深度缓冲区为24bit
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    [EAGLContext setCurrentContext:self.context];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
-
 
 @end
